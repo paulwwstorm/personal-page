830bf4262dadcf99e1f20ad80a17d9882788e44c
paulwwstorm <paulwwstorm@gmail.com>
2023-11-06 13:19:15 +0100

    First steps personalization before upload to AWS

--- homepage/src/App.tsx
+++ homepage/src/App.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
+import photo from './selfie.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={photo} className="App-logo" alt="it's me" />
+        <div>
+          Hello! My name is Paul Storm. I am a fullstack software developer.
+        </div>
+        <div>
+          I am planning on using this page to organize and track my projects.
+        </div>
       </header>
     </div>
   );
