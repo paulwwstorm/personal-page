7606f23dfaf6d864386147ac4d5d337616ba3178
paulwwstorm <paulwwstorm@gmail.com>
2023-11-08 15:50:58 +0100

    Updating intro to include link to #Chinese demo

--- homepage/src/App.tsx
+++ homepage/src/App.tsx
@@ -2,17 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import photo from './selfie.jpg'
 import './App.css';
+import Image from 'react-bootstrap/Image';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={photo} className="App-logo" alt="it's me" />
+        {/* <Col xs={6} md={4}> */}
+        <Image
+          src={photo}
+          alt="it's me"
+          rounded
+          fluid
+          height="300vmin"
+          />
+        {/* </Col> */}
         <div>
           Hello! My name is Paul Storm. I am a fullstack software developer.
         </div>
         <div>
-          I am planning on using this page to organize and track my projects.
+          This page is a work in progress, click <a href="https://www.youtube.com/watch?v=pmqzUk7WuUk&feature=youtu.be">here</a> for a demo of a previous full-stack application I built
         </div>
       </header>
     </div>
