f0c69ebdf1707a6b188bd8ead9ffcc01cb1fabda
Paul Storm <paulwwstorm@gmail.com>
2024-02-19 11:52:56 -0600

    Updating css, found out why Bootstrap components were not working.

--- homepage/src/components/Home.tsx
+++ homepage/src/components/Home.tsx
@@ -3,26 +3,37 @@ import '../App.css';
 import NavBar from './NavBar';
 import Image from 'react-bootstrap/Image';
 import photo from '../selfie.jpg'
+import { Col, Container, Row } from 'react-bootstrap';
 
 
 const Home: React.FC = () => {
     return (
-        <div className="App">
+        <div 
+        className="App"
+        >
             <NavBar/>
-            <header className="App-header">
-            <Image
-                src={photo}
-                alt="it's me"
-                rounded
-                fluid
-                height="300vmin"
-                />
-            <div>
-                Hello! My name is Paul Storm. I am a fullstack software developer.
-            </div>
-            <div>
-                This page is a work in progress, click <a href="https://www.youtube.com/watch?v=pmqzUk7WuUk&feature=youtu.be">here</a> for a demo of a previous full-stack application I built
-            </div>
+            <header 
+            className="App-header"
+            >
+                <Container fluid>
+                    <Row>
+                        <Col xs={6} md={4}>
+                            <Image
+                                src={photo}
+                                alt="it's me"
+                                height="300vmin"
+                                text-align="center"
+                                roundedCircle
+                                />
+                        </Col>
+                    </Row>
+                    <div>
+                        Hello! My name is Paul Storm. I am a fullstack software developer.
+                    </div>
+                    <div>
+                        This page is a work in progress, click <a href="https://www.youtube.com/watch?v=pmqzUk7WuUk&feature=youtu.be">here</a> for a demo of a previous full-stack application I built
+                    </div>
+                </Container>
             </header>
         </div>
     )
