a942ee3638fb681d1576493fbcc11ea1503edef8
Paul Storm <paulwwstorm@gmail.com>
2024-02-20 10:25:58 -0600

    Fun with css and Bootstrap...

--- homepage/src/components/Home.tsx
+++ homepage/src/components/Home.tsx
@@ -8,33 +8,30 @@ import { Col, Container, Row } from 'react-bootstrap';
 
 const Home: React.FC = () => {
     return (
-        <div 
-        className="App"
-        >
-            <NavBar/>
-            <header 
-            className="App-header"
-            >
-                <Container fluid>
-                    <Row>
-                        <Col xs={6} md={4}>
-                            <Image
-                                src={photo}
-                                alt="it's me"
-                                height="300vmin"
-                                text-align="center"
-                                roundedCircle
-                                />
-                        </Col>
-                    </Row>
-                    <div>
+        <div className="App">
+            {/* <NavBar/> */}
+            <Container className="vh-100" >
+                <Row className="my-2 justify-content-md-center">
+                    <Col xs={6} md={4}>
+                        <Image
+                            src={photo}
+                            alt="it's me"
+                            height="300vmin"
+                            roundedCircle
+                            />
+                    </Col>
+                </Row>
+                <Row className='my-100'>
+                    <Col>
                         Hello! My name is Paul Storm. I am a fullstack software developer.
-                    </div>
-                    <div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
                         This page is a work in progress, click <a href="https://www.youtube.com/watch?v=pmqzUk7WuUk&feature=youtu.be">here</a> for a demo of a previous full-stack application I built
-                    </div>
-                </Container>
-            </header>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
